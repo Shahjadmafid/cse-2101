787ec141d76d469b65c733cae6a35df1347c63fc
Shahjadmafid <fazlarabby221281@gmail.com>
2025-12-13 13:58:20 +0600

    13-12

---
+++ bubble_sort_desending_order.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[5] = {2, 4, 1, 5, 7};
+    int t;
+
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            if(a[j] < a[j+1])   
+            {
+                t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
+
+    for(int i = 0; i < 5; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
